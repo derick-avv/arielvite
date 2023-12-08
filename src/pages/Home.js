32e9f55ed64b544f70504c8d9a9f8995a902edc5
Derick Vasquez <derickavv@gmail.com>
2023-12-08 16:15:43 -0500

    Success

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Home() {
+  useEffect(() => {
+    let slideCount = 1;
+  }, []);
+  const handleArrowClick = () => {};
+
   return (
     <>
       <div className="section" data-num="1">
@@ -13,118 +18,20 @@ function Home() {
         </div>
       </div>
       <div className="section" data-num="2">
-        <div className="cards">
-          <div className="flipCard">
-            <div className="flipCardInner">
-              <div className="flipCardFront">
-                <div className="cardImage">
-                  <i className="fa-solid fa-briefcase-medical"></i>
-                </div>
-              </div>
-              <div className="flipCardBack">
-                <div className="cardImage ">
-                  <i className="fa-solid fa-briefcase-medical"></i>
-                </div>
-                <div className="cardTitle">
-                  <h3>Life and Health Insurance Sales</h3>
-                </div>
-                <div className="cardInfo1">
-                  <p>Life, Accident and Health Pre-Licensing</p>
-                  <div className="status">
-                    <span className="statusDotPen"></span>
-                    <p className="statusNamePen">Pending</p>
-                  </div>
-                </div>
-                <div className="cardInfo2">
-                  <p>Life and Health Insurance License</p>
-                  <div className="status">
-                    <span className="statusDotPen"></span>
-                    <p className="statusNamePen">Pending</p>
-                  </div>
-                </div>
-                <div className="cardInfo3">
-                  <p>Social Security Certification</p>
-                  <div className="status">
-                    <span className="statusDotPen"></span>
-                    <p className="statusNamePen">Pending</p>
-                  </div>
-                </div>
-
-                <NavLink className="cardInfo4" to="/blog">
-                  <div>
-                    Keep up with my journey through Bankers Life & Casualty Co.
-                  </div>
-                  <div className="arrowToBlog">
-                    <i class="fa-solid fa-arrow-right"></i>
-                  </div>
-                </NavLink>
-              </div>
-            </div>
+        <h1 className="pageTitle">Expertise</h1>
+        <div className="slider">
+          <div className="prev" onClick={handleArrowClick}>
+            &#10094;
           </div>
-          <div className="flipCard">
-            <div className="flipCardInner">
-              <div className="flipCardFront">
-                <div className="cardImage">
-                  <i class="fa-solid fa-house-chimney"></i>
-                </div>
-              </div>
-              <div className="flipCardBack">
-                <div className="cardImage ">
-                  <i class="fa-solid fa-house-chimney"></i>
-                </div>
-                <div className="cardTitle">
-                  <h3>Real Estate Wholesaling & Flipping</h3>
-                </div>
-              </div>
-            </div>
+          <div className="next" onClick={handleArrowClick}>
+            &#10095;
           </div>
-          <div className="flipCard">
-            <div className="flipCardInner">
-              <div className="flipCardFront">
-                <div className="cardImage">
-                  <i class="fa-solid fa-file-code"></i>
-                </div>
-              </div>
-              <div className="flipCardBack">
-                <div className="cardImage ">
-                  <i class="fa-solid fa-file-code"></i>
-                </div>
-                <div className="cardTitle">
-                  <h3>Fullstack Dev & Marketing</h3>
-                </div>
-                <div className="cardInfo1">
-                  <p>Javascript Certification</p>
-                  <div className="status">
-                    <span className="statusDotAcq"></span>
-                    <p className="statusNameAcq">Acquired</p>
-                  </div>
-                </div>
-                <div className="cardInfo2">
-                  <p>Advanced React Certification</p>
-                  <div className="status">
-                    <span className="statusDotAcq"></span>
-                    <p className="statusNameAcq">Acquired</p>
-                  </div>
-                </div>
-                <div className="cardInfo3">
-                  <p>PHP 8 & MySQL Certification</p>
-                  <div className="status">
-                    <span className="statusDotAcq"></span>
-                    <p className="statusNameAcq">Acquired</p>
-                  </div>
-                </div>
-                <NavLink className="cardInfo4" to="/blog">
-                  <div>
-                    Take a look at some of my many Frontend and Fullstack
-                    projects.
-                  </div>
-                  <div className="arrowToBlog">
-                    <i class="fa-solid fa-arrow-right"></i>
-                  </div>
-                </NavLink>
-              </div>
-            </div>
-          </div>
+          <div className="slide"></div>
+        </div>
+        <div className="dots">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
         </div>
       </div>
       <div className="section" data-num="3">
